37f17d6f6e9f9d1403a9983733e1e361359f7839
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2023-05-04 19:18:08 +0900

    [Bronze III] Title: 윷놀이, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num[] = new int[4];
+        int sum[] = new int[3];
+
+        for(int i=0; i<3; i++){
+            sum[i] = 0; // 초기화
+            for(int j=0; j<4; j++){
+                num[j] = sc.nextInt();
+                sum[i] = sum[i] + num[j];
+            }
+        }
+        for(int i=0; i<3; i++){
+            if(sum[i] == 4){
+                System.out.println("E");
+            }
+            else if(sum[i] == 3){
+                System.out.println("A");
+            }
+            else if(sum[i] == 2){
+                System.out.println("B");
+            }
+            else if(sum[i] == 1){
+                System.out.println("C");
+            }
+            else{
+                System.out.println("D");
+            }
+        }
+    }
+}
