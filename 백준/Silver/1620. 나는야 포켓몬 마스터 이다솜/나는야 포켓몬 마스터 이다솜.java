020ad1ff2c1eddb7e8d8619f2f6045af2d9532ae
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2023-05-19 21:49:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1656 ms, Memory: 181284 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+        public static boolean isNumeric(String s) {
+            try {
+                Double.parseDouble(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            
+            StringBuilder sb = new StringBuilder();
+
+            HashMap<Integer, String> hm = new HashMap<>();
+            HashMap<String, Integer> hm2 = new HashMap<>();
+
+            for(int i =1;i < n + 1;i++){
+                String s = sc.next();
+                hm.put(i, s);
+                hm2.put(s, i);
+            }
+
+            for(int i = 0;i < m;i++){
+                String in = sc.next();
+                if(isNumeric(in)){
+                    sb.append(hm.get(Integer.parseInt(in))).append("\n");
+                }else{
+                    sb.append(hm2.get(in)).append("\n");
+                }
+
+            }
+
+            System.out.println(sb);
+    }
+}
