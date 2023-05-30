c24da33cd533d6f2a8ba418811c5bc230fb8347c
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2023-05-30 11:03:56 +0900

    [Silver IV] Title: ATM, Time: 268 ms, Memory: 20296 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+
+
+        for(int i = 0;i <n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int temp = 0;
+        int sum = 0;
+
+        for(int i = 0;i < arr.length;i++){
+            sum += temp + arr[i];
+            temp += arr[i];
+        }
+        System.out.println(sum);
+
+    }
+}
