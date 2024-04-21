3ffa1703818623f6a20719bebf81c33602367517
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-04-21 20:00:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,37 @@
+import java.lang.*;
+
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = Long.valueOf(num);
+        int cnt = 0;
+        
+        if(n == 1){
+          return 0;  
+        }else{
+            for(int i = 0; i <= 500; i++){
+                if(n == 1){
+                    cnt = i;
+                    break;
+                } 
+                if((n % 2) == 0){
+                    n /= 2;
+                    cnt++;
+                }else{
+                    n = (n * 3) + 1;
+                    cnt++;
+                }
+                
+
+            }
+        }
+        
+        if(cnt >= 500){
+            answer = -1;
+        }else{
+            answer = cnt;
+        }
+            
+        return answer;
+    }
+}
