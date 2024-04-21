6a5221ada57d1aac717f7dd5f8bc6337f25c0c35
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-04-22 00:54:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.23 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -3,30 +3,47 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] list = s.split(" ", -1);
+//         String[] list = s.split(" ", -1);
 
-        for(int i = 0; i < list.length; i++){
-            String word = list[i];
-            String newStr = "";
-            for(int j = 0; j < word.length(); j++){
-                char temp = word.charAt(j);
+//         for(int i = 0; i < list.length; i++){
+//             String word = list[i];
+//             String newStr = "";
+//             for(int j = 0; j < word.length(); j++){
+//                 char temp = word.charAt(j);
 
-                if((j % 2) == 0){
-                    newStr += Character.toUpperCase(temp);
+//                 if((j % 2) == 0){
+//                     newStr += Character.toUpperCase(temp);
+//                 }else{
+//                     newStr += Character.toLowerCase(temp);;
+//                 }
+//             }
+//             newStr = newStr + " ";
+//             if(i == list.length-1){
+//                 newStr = newStr.trim();
+//                 answer += newStr;
+//             }else{
+//                 answer += newStr;
+//             }
+    
+//         }
+                
+//         return answer;
+            int cnt = 0;
+            for(int i = 0; i<s.length(); i++){
+               char ch = s.charAt(i);
+                if(ch == ' '){
+                    answer += " ";
+                    cnt = 0;
+                    continue;
+                }
+                if((cnt%2) == 0){
+                    answer += String.valueOf(Character.toUpperCase(ch));
+                    cnt++;
                 }else{
-                    newStr += Character.toLowerCase(temp);;
+                    answer += String.valueOf(Character.toLowerCase(ch));
+                    cnt++;
                 }
             }
-            newStr = newStr + " ";
-            if(i == list.length-1){
-                newStr = newStr.trim();
-                answer += newStr;
-            }else{
-                answer += newStr;
-            }
-    
-        }
-                
-        return answer;
+            return answer;
     }
 }
