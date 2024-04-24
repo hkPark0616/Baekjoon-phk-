d6f128082ca77a48bb315d93083206dbccb67392
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-04-25 02:24:16 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length]; 
+        
+        for(int i = 0; i < commands.length; i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int num = commands[i][2];
+            
+            int[] temp = new int[end - start + 1]; 
+            
+            for(int j = 0; j <=  end - start; j++){
+                temp[j] = array[j + start - 1];
+            }
+            
+            Arrays.sort(temp);
+            
+            answer[i] = temp[num - 1];
+
+        }
+        return answer;
+    }
+}
