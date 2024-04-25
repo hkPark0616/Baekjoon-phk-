0568462a3a0a597b01f3cef3796c1654f7d7a0c1
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-04-25 14:44:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 14.07 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i = 1; i < food.length; i++){
+            int n = food[i] / 2;
+            for(int j = 0; j < n; j++){
+                answer += i;
+            }
+        }
+        answer += "0";
+        
+        for(int i = food.length - 1; i > 0; i--){
+            int n = food[i] / 2;
+            for(int j = 0; j < n; j++){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
