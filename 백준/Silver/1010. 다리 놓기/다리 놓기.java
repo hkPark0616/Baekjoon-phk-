9965b063831ea967fae601718ec07ae46366e906
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-06-30 08:38:07 +0900

    [Silver V] Title: 다리 놓기, Time: 220 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
+	
+	public static int dp[][] = new int[30][30];
+	
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t = 0; t < T; t++) {
+			
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			// mCn
+			int result = 1;
+			for (int i = 1; i <= N; i++) {
+				result = result * (M - i + 1) / i;
+			}
+			
+			System.out.println(result);
+			
+			// dp
+			//System.out.println(comb(M, N));
+			
+			
+		}
+		
+	}
+	
+	// dp
+	public static int comb(int n, int r) {
+		if(dp[n][r] > 0) {
+			return dp[n][r];
+		}
+		
+		if(n == r || r == 0) {
+			return dp[n][r] = 1;
+		}
+		
+		return comb(n - 1, r - 1) + comb(n - 1, r);
+	}
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            int result;
-            for(int i = 0;i < t;i++){
-                int n = sc.nextInt();
-                int m = sc.nextInt();
-
-                if(n == m){
-                    System.out.println(1);
-                }else if(n == 1){
-                    System.out.println(m);
-                }else{
-                    result = 1;
-                    for(int j = 0;j < n;j++){
-                        result = result * (m - j) / (j+1);
-                    }
-                    System.out.println(result);
-                }
-            }
-
-
-    }
 }
