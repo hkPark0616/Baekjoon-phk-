1b7274e245e86efecb3e21a471ae19f812426518
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-06-30 10:22:27 +0900

    Update 크로아티아 알파벳.java

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -11,16 +11,20 @@ public class Main {
 		int cnt = 0;
 		
 		for(int i = 0; i < arr.length; i++) {
+//			if(s.contains(arr[i])) {
+//				s = s.replaceFirst(arr[i], " ");
+//				//s = s.replace(" ", "");
+//				cnt++;
+//				i--;
+//			}
 			if(s.contains(arr[i])) {
-				s = s.replaceFirst(arr[i], " ");
-				//s = s.replace(" ", "");
-				cnt++;
-				i--;
+				s = s.replace(arr[i], "@");
 			}
 			
 		}
-		s = s.replace(" ", "");
-		cnt += s.length();
+//		s = s.replace(" ", "");
+//		cnt += s.length();
+		cnt = s.length();
 
 		
 		System.out.println(cnt);
