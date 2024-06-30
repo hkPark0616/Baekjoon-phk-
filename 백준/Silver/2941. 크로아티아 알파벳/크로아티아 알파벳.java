946ee23f488723565852d452d4f1660137736b0f
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-06-30 10:18:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		String arr[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+		int cnt = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(s.contains(arr[i])) {
+				s = s.replaceFirst(arr[i], " ");
+				//s = s.replace(" ", "");
+				cnt++;
+				i--;
+			}
+			
+		}
+		s = s.replace(" ", "");
+		cnt += s.length();
+
+		
+		System.out.println(cnt);
+		
+	}
+
+}
