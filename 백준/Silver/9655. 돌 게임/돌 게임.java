323db2d40bccd230bf75d1daea3a1dfb41e346dc
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-07-29 22:19:59 +0900

    [Silver V] Title: 돌 게임, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] dp = new int[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 1;
+		
+		for(int i = 4; i <= n; i++) {
+			dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+		}
+		
+		if(dp[n] % 2 == 0) {
+			System.out.println("CY");
+		}else {
+			System.out.println("SK");
+		}
+			
+	}
+}
