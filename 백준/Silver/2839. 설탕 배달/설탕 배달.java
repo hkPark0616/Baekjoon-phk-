93101d9ac900d442005f5e8ab48e685736ad2155
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-08-20 10:03:41 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int cnt = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int answer = 0;
+		
+		while(true) {
+			
+			if(N % 5 == 0) {
+				answer = answer + N / 5;
+				break;
+			}else if(N < 0){
+				answer = -1;
+				break;
+			}
+			
+			N -= 3;
+			answer++;
+		}
+		
+		System.out.println(answer);
+	}
 
-        while(true){
-
-            if(n % 5 ==0){
-                System.out.println(n/5 + cnt);
-                break;
-            }
-            else if(n < 0){
-                System.out.println(-1);
-                break;
-            }
-            n -= 3;
-            cnt++;
-        }
-
-
-    }
 }
