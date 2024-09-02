194e617acbfe883a0b5c83820e45ae2c1b636223
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-09-02 15:26:50 +0900

    [Gold IV] Title: 연구소, Time: 1076 ms, Memory: 300200 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,132 +1,98 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
-
+import java.util.StringTokenizer;
 
 public class Main {
-
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static List<Node> newWall = new ArrayList<>();
-	static List<Node> virus = new ArrayList<>();
-	
-	static final int[] dx = {-1,1,0,0};
-	static final int [] dy = {0,0,-1,1};
-	
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		int n = read();
-		int m = read();
+	static int N, M, map[][], answer;
+	static int[] delX = {-1, 0, 1, 0}, delY = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int [][] matrix = new int[n][m];
-		for (int i=0;i<n;i++) {
-			for (int j=0;j<m;j++) {
-				matrix[i][j] = read(); // 공백 및 \n 기준으로 쭉 입력
-				
-				if (matrix[i][j]==0) {
-					newWall.add(new Node(i,j));
-					// 벽이 새롭게 깔릴 수 있는 곳
-				}
-				
-				if (matrix[i][j]==2) {
-					virus.add(new Node(i,j));
-				}
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		answer = 0;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				int n = Integer.parseInt(st.nextToken());
+				map[i][j] = n;
 			}
-		}// end data input
-		
-		int max =0;
-		
-		// 모든 곳에 벽을 세우고 확산 시뮬레이션
-		for (int i=0; i <newWall.size(); i++) { // 벽의 개수만큼
-			for (int j =i+1; j<newWall.size(); j++) {
-				for( int k=j+1; k<newWall.size();k++) {
-					int[][] newMatrix = matrixClone(matrix, i,j,k);
-					// 기존 빈구역의 갯수 - 새로생긴 벽 1,2,3 - 확산된 바이러스 개수
-					max = Math.max(max,  newWall.size()-3-BFS(newMatrix));
-				}
-			}
-			
-		}
-			
-	
-		System.out.println(max);
-			
-			
-	} // end main
-	
-	//추가되는 바이러스의 수
-	public static int BFS(int[][] matrix) {
-		int count=0;
-		Queue<Node> q = new ArrayDeque<>(virus);
-		
-		
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			
-			for (int i=0; i<4;i++) {
-				int nx = node.x + dx[i];
-				int ny = node.y + dy[i];
-				
-				
-				if (nx <0 || ny < 0 || nx >= matrix.length || ny >= matrix[0].length) continue;
-				if (matrix[nx][ny] != 0) continue;
-				
-				matrix[nx][ny] = 2;
-				count ++;
-				q.add(new Node(nx,ny)); 
-				
-			}
-		} // end bfs
-		
-		return count;
-	}  // end func
-	
-	
-	
-	public static int[][] matrixClone(int[][] matrix, int i, int j, int k) {
-		int [][] newMatrix = new int[matrix.length][matrix[0].length];
-		for (int row = 0; row < matrix.length;row++) {
-			newMatrix[row] = matrix[row].clone();
 		}
 		
-		newMatrix[newWall.get(i).x][newWall.get(i).y] =1;
-		newMatrix[newWall.get(j).x][newWall.get(j).y] =1;
-		newMatrix[newWall.get(k).x][newWall.get(k).y] =1;
+		wall(0);
 		
-		return newMatrix;
+		System.out.println(answer);
 	}
 	
-	public static int read() throws IOException {
-		int res = 0;
-		while (true) {
-			int r = br.read();
-			
-			if (r ==10 || r == 32) return res; // 10 공백 혹은 32 \n 인 경우 종료
-			if (r==13) continue;
-			res = 10 * res + r - 48; // 나온 수들을 순서대로 int로 바꿔서 정렬 // 당연히 0도 입력
+	static void wall(int cnt) {
+		if(cnt == 3) {
+			int[][] copy = new int[N][M];
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < M; j++) {
+					copy[i][j] = map[i][j];
+				}
+			}
+			spreadVirus(copy);
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(map[i][j] == 0) {
+					map[i][j] = 1;
+					wall(cnt + 1);
+					map[i][j] = 0;
+					
+				}
+			}
 		}
 	}
 	
-	
-	
-	
-	
-	// 좌표를 받는 객체 생성
-	public static class Node {
-		int x,y;
-		
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
+	static void spreadVirus(int[][] copyArr) {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(copyArr[i][j] == 2) {
+					Queue<int[]> queue = new LinkedList<>();
+					queue.offer(new int[] {i, j});
+					
+					while(!queue.isEmpty()) {
+						int[] temp = queue.poll();
+						int x = temp[0];
+						int y = temp[1];
+						
+						for(int d = 0; d < 4; d++) {
+							int nx = x + delX[d];
+							int ny = y + delY[d];
+							
+							if(nx >= 0 && nx < N && ny >= 0 && ny < M && copyArr[nx][ny] == 0) {
+								copyArr[nx][ny] = 2;
+								queue.offer(new int[] {nx, ny});
+							}
+						}
+					}	
+				}
+			}
 		}
+		
+		calcSafeArea(copyArr);
 	}
 	
-
+	static void calcSafeArea(int[][] copyArr) {
+		int cnt = 0;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				if(copyArr[i][j] == 0) cnt++;
+			}
+		}
+		
+		answer = Math.max(cnt, answer);
+	}
 }
