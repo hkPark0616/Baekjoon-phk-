3eac86a79b8e76a0d7901b2c8f165e4bf4ae0dd6
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-09-22 22:00:44 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 280 ms, Memory: 22264 KB, Score: 11 point -BaekjoonHub

--- 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,6 +8,7 @@ public class Main {
     static String[][] status;
     static int[][] deltas = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -19,17 +17,11 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         map = new int[N][M];
         status = new String[N][M];
-        scores = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                status[i][j] = "S";
-            }
-        }
-
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                status[i][j] = "S";
             }
         }
 
@@ -52,7 +44,8 @@ public class Main {
                     attack(attackX, attackY, 3);
                     break;
             }
-            
+
+
             st = new StringTokenizer(br.readLine());
             int defendX = Integer.parseInt(st.nextToken());
             int defendY = Integer.parseInt(st.nextToken());
@@ -64,32 +57,35 @@ public class Main {
     }
 
     static void attack(int a, int b, int d){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {a, b, map[a][b]});
-
-        while (!queue.isEmpty()){
-            int[] temp = queue.poll();
-            int x = temp[0];
-            int y = temp[1];
-            int h = temp[2];
-
-            if(map[x][y] == 0) continue;
-
-            map[x][y] = 0;
-            status[x][y] = "F";
+        if (status[a][b].equals("F")) { // 이미 방문한 칸이면 종료
+            return;
+        } else {
+            int size = map[a][b] - 1;
+            status[a][b] = "F";
             scores++;
 
-            int nx = x;
-            int ny = y;
-            for(int i = 1; i < h; i++){
-                nx += deltas[d][0];
-                ny += deltas[d][1];
+            while (size > 0) {
+                int nx = a + deltas[d][0];
+                int ny = b + deltas[d][1];
 
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                if(map[nx][ny] != 0){
-                    status[nx][ny] = "F";
-                    queue.offer(new int[] {nx, ny, map[nx][ny]});
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                    return;
                 }
+                size--;
+                a = nx;
+                b = ny;
+
+                if (status[nx][ny].equals("F")) {
+                    continue;
+                }
+                // 안넘어뜨린칸
+                status[nx][ny] = "F";
+
+                // 새로 방문한 곳과 기존 도미노 사이즈 비교
+                size = Math.max(size, map[nx][ny] - 1);
+
+                //방문 횟수 증가
+                scores++;
             }
         }
     }
@@ -97,6 +93,8 @@ public class Main {
     static void defend(int x, int y){
         if(status[x][y].equals("F")){
             status[x][y] = "S";
+        }else{
+            return;
         }
     }
 
