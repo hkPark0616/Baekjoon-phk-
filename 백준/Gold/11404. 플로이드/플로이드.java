473ed9d5ab786b26a7274139a6fbc5e020de53a4
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-10-16 19:47:19 +0900

    [Gold IV] Title: 플로이드, Time: 428 ms, Memory: 45536 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int INF = 1000000000;
+    static int[][] graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(i == j) graph[i][j] = 0;
+                else graph[i][j] = INF;
+            }
+        }
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = Math.min(graph[a][b], c);
+        }
+
+        floyd();
+
+        print();
+    }
+
+    static void floyd(){
+        for(int k = 0; k < N; k++) {
+            for(int i = 0; i < N; i++) {
+                if(i == k) continue;
+                for(int j = 0; j < N; j++) {
+                    if(i == j || j == k) continue;
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+    }
+
+    static void print() {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(graph[i][j] == INF) System.out.print("0 ");
+                else System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
