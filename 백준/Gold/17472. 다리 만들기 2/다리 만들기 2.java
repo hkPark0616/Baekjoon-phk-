5fef266eff3ecdace8b303c8f650a67edb6afac9
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-11-27 14:33:07 +0900

    [Gold I] Title: 다리 만들기 2, Time: 68 ms, Memory: 11736 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -7,13 +7,14 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, parent[], map[][];
-    static boolean visited[][];
-    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] parent;
     static PriorityQueue<Edge> pq;
-    static int island;
-    static int bridgeCnt;
-    static int answer;
+    static int island, bridgeCount, answer;
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
     static class Edge implements  Comparable<Edge>{
         int a, b, w;
 
@@ -39,43 +40,41 @@ public class Main {
         map = new int[N][M];
         visited = new boolean[N][M];
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
+            for(int j = 0; j < M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 섬 구분
         island = 0;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(map[i][j] == 1 && !visited[i][j]) {
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(map[i][j] == 1 && !visited[i][j]){
                     island++;
                     bfs(i, j);
                 }
             }
         }
 
-        // 다리 만들기
-        pq = new PriorityQueue<Edge>();
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(map[i][j] != 0) {
+        pq = new PriorityQueue<>();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(map[i][j] != 0){
                     findBridge(i, j, map[i][j]);
                 }
             }
         }
 
         parent = new int[island + 1];
-        for(int i = 0; i < parent.length; i++) {
+        for(int i = 0; i < parent.length; i++){
             parent[i] = i;
         }
 
-        int size = pq.size();
-        bridgeCnt = 0;
         answer = 0;
-        for(int i = 0; i < size; i++) {
+        bridgeCount = 0;
+        int size = pq.size();
+        for(int i = 0; i < size; i++){
             Edge e = pq.poll();
 
             int a = find(e.a);
@@ -84,15 +83,16 @@ public class Main {
             if(a != b){
                 union(a, b);
                 answer += e.w;
-                bridgeCnt++;
+                bridgeCount++;
             }
         }
 
-        if(answer == 0 || bridgeCnt != island - 1){
+        if(answer == 0 || bridgeCount != island - 1){
             System.out.println("-1");
         }else{
             System.out.println(answer);
         }
+
     }
 
     static void union(int a, int b){
@@ -118,7 +118,7 @@ public class Main {
         map[a][b] = island;
         q.offer(new int[]{a, b});
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
@@ -127,49 +127,35 @@ public class Main {
                 int nx = x + deltas[d][0];
                 int ny = y + deltas[d][1];
 
-                if(nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && map[nx][ny] == 1) {
+                if(nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && map[nx][ny] == 1){
                     visited[nx][ny] = true;
                     map[nx][ny] = island;
-                    q.offer(new int[]{nx, ny});
+                    q.add(new int[]{nx, ny});
+
                 }
             }
         }
     }
 
     static void findBridge(int a, int b, int num){
-        int x = a;
-        int y = b;
-        int length = 0;
+        for(int d = 0; d < 4; d++){
+            int x = a;
+            int y = b;
+            int length = 0;
 
-        for(int i = 0; i < 4; i++){
-            while (true){
-                x = x + deltas[i][0];
-                y = y + deltas[i][1];
+            while(true){
+                x += deltas[d][0];
+                y += deltas[d][1];
 
-                if(x >= 0 && x < N && y >= 0 && y < M) {
-                    if (map[x][y] == num) {
-                        length = 0;
-                        x = a;
-                        y = b;
-                        break;
-                    } else if (map[x][y] == 0) {
-                        length++;
-                    } else {
-                        if (length > 1) {
-                            pq.offer(new Edge(num, map[x][y], length));
-                        }
-
-                        length = 0;
-                        x = a;
-                        y = b;
-                        break;
+                if(x < 0 || x >= N || y < 0 || y >= M) break;
+                if(map[x][y] == num) break;
+                if(map[x][y] > 0){
+                    if(length > 1){
+                        pq.offer(new Edge(num, map[x][y], length));
                     }
-                }else{
-                    length = 0;
-                    x = a;
-                    y = b;
                     break;
                 }
+                length++;
             }
         }
     }
