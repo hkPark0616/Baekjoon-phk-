c3c01057d97dc21ea500588f270b5a9da8e94d05
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-11-29 21:02:28 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 2,416 ms, Memory: 21,040 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,74 +1,73 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, map[][], min;
-	static int arr[];
-	static boolean visited[];
-	static int workX, workY, homeX, homeY;
-	static StringBuilder sb = new StringBuilder();
+    static int N, answer;
+    static int[][] house;
+    static int[] arr;
+    static boolean[] selected;
+    static int startX, startY, endX, endY;
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		
-		for(int t = 1; t <= T; t++) {
-			
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			
-			map = new int[N][2];
-			
-			min = Integer.MAX_VALUE;
-			
-			arr = new int[N];
-			visited = new boolean[N];
-			
-			st = new StringTokenizer(br.readLine());
-			
-			
-			workX = Integer.parseInt(st.nextToken());
-			workY = Integer.parseInt(st.nextToken());
-			homeX = Integer.parseInt(st.nextToken());
-			homeY = Integer.parseInt(st.nextToken());
-			
-			for(int i = 0; i < N; i++) {
-				int cx = Integer.parseInt(st.nextToken());
-				int cy = Integer.parseInt(st.nextToken());
-				map[i][0] = cx;
-				map[i][1] = cy;
-				
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			path(0);
-			
-			sb.append("#" + t + " " + min + "\n");
+        int T = Integer.parseInt(st.nextToken());
 
-		}
-		System.out.println(sb);
-	}
+        for(int t = 1; t <= T; t++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            answer = Integer.MAX_VALUE;
+            house = new int[N][2];
+            arr = new int[N];
+            selected = new boolean[N];
 
-	static void path(int cnt) {
-		if(cnt == N) {
-			int meet = Math.abs(workX - map[arr[0]][0]) + Math.abs(workY - map[arr[0]][1]);
-			int gohome = Math.abs(homeX - map[arr[N - 1]][0]) + Math.abs(homeY - map[arr[N - 1]][1]);
-			int sum = 0;
-			for(int i = 0; i < N - 1; i++) {
-				sum += (Math.abs(map[arr[i]][0] - map[arr[i + 1]][0]) + Math.abs(map[arr[i]][1] - map[arr[i + 1]][1]));
-			}
+            st = new StringTokenizer(br.readLine());
+            startX = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
+            endX = Integer.parseInt(st.nextToken());
+            endY = Integer.parseInt(st.nextToken());
 
-			min = Math.min(min, sum + meet + gohome);
-		}
-		
-		for(int i = 0; i < N; i++) {
-			if(visited[i]) continue;
-			
-			arr[cnt] = i;
-			visited[i] = true;
-			path(cnt + 1);
-			visited[i] = false;
-		}
-	}
+            for(int i = 0; i < N; i++){
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                house[i][0] = a;
+                house[i][1] = b;
+            }
+
+
+
+            perm(0);
+
+            System.out.println("#" + t + " " + answer);
+        }
+    }
+
+    static void perm(int cnt){
+        if(cnt == N){
+            int result = 0;
+
+            result += Math.abs(startX - house[arr[0]][0]) + Math.abs(startY - house[arr[0]][1]);
+            for(int i = 0; i < N - 1; i++){
+                result += Math.abs(house[arr[i]][0]- house[arr[i + 1]][0]) + Math.abs(house[arr[i]][1] - house[arr[i + 1]][1]);
+            }
+            result += Math.abs(endX - house[arr[N - 1]][0]) + Math.abs(endY- house[arr[N - 1]][1]);
+
+            answer = Math.min(result, answer);
+
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!selected[i]){
+                selected[i] = true;
+                arr[cnt] = i;
+                perm(cnt + 1);
+                selected[i] = false;
+            }
+        }
+    }
 }
