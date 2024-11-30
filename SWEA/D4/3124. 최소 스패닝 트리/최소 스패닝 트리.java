5ea4982ea59aeec130a48106856db989754b206e
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-01 00:57:34 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,045 ms, Memory: 117,700 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,91 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
- class Node implements Comparable<Node>{
-	int to;
-	int from;
-	int weight;
-	
-	public Node(int to, int from, int weight) {
-		this.to = to;
-		this.from = from;
-		this.weight = weight;
-	}
-	
-	@Override
-	public int compareTo(Node node) {
-		return weight - node.weight;
-	}
-}
+import java.util.*;
 
 public class Solution {
-	static int V, E;
-	static int[] parent;
-	static PriorityQueue<Node> pq;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		
-		for(int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			pq = new PriorityQueue<>();
-			
-			parent = new int[V + 1];
-			for(int i = 0; i <= V; i++) {
-				parent[i] = i;
-			}
-			
-			for(int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int w = Integer.parseInt(st.nextToken());
-				
-				pq.add(new Node(a, b, w));
-			}
-			
-			int size = pq.size();
+    static int V, E, parent[];
+    static long answer;
+    static PriorityQueue<Edge> pq;
+    static class Edge implements Comparable<Edge>{
+        int a, b, w;
 
-			long sum = 0;
-			for(int i = 0; i < size; i++) {
-				Node node = pq.poll();
-		
-				if(!isSame(node.to, node.from)) {
-					sum += node.weight;
-					union(node.to, node.from);
-				}
-			}
-			
-			System.out.println("#" + t + " " + sum);
-		}
+        public Edge(int a, int b, int w){
+            this.a = a;
+            this.b = b;
+            this.w = w;
+        }
 
-	}
-	
-	static void union(int a, int b) {
-		int aRoot = parent[a];
-		int bRoot = parent[b];
-		
-		if(aRoot != bRoot) {
-			parent[bRoot] = aRoot;
-		}
-	}
+        @Override
+        public int compareTo(Edge o1) {
+            return this.w - o1.w;
+        }
+    }
 
-	static int find(int a) {
-		if(parent[a] == a) return parent[a];
-		
-		return parent[a] = find(parent[a]);
-	}
-	
-	static boolean isSame(int a, int b) {
-		return find(a) == find(b);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int T = Integer.parseInt(st.nextToken());
+
+        for(int t = 1; t <= T; t++){
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+
+            parent = new int[V + 1];
+            for(int i = 0; i <= V; i++)
+                parent[i] = i;
+
+            pq = new PriorityQueue<>();
+            for(int i = 0; i < E; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int w = Integer.parseInt(st.nextToken());
+                pq.offer(new Edge(a, b, w));
+            }
+
+            int size = pq.size();
+            answer = 0;
+            for(int i = 0; i < size; i++){
+                Edge e = pq.poll();
+
+                if(!isSame(e.a, e.b)){
+                    union(e.a, e.b);
+                    answer += e.w;
+                }
+            }
+
+            System.out.println("#" + t + " " + answer);
+        }
+    }
+
+    static void union(int a, int b){
+        a = parent[a];
+        b = parent[b];
+
+        if(a != b) {
+            parent[b] = a;
+        }
+    }
+
+    static int find(int a){
+        if(a == parent[a]){
+            return parent[a];
+        }
+
+        return parent[a] = find(parent[a]);
+    }
+
+    static boolean isSame(int a, int b){
+        return find(a) == find(b);
+    }
 }
