c69392e5864f79a9c53bd74dc18fbd8c720d968d
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-02 11:44:51 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,970 ms, Memory: 21,048 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int N, answer;
-    static int[][] house;
-    static int[] arr;
-    static boolean[] selected;
+    static int[][] houses;
     static int startX, startY, endX, endY;
+    static int[] arr;
+    static boolean[] isSelected;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,53 +19,59 @@ public class Solution {
         for(int t = 1; t <= T; t++){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            answer = Integer.MAX_VALUE;
-            house = new int[N][2];
+
+            houses = new int[N][2];
             arr = new int[N];
-            selected = new boolean[N];
+            isSelected = new boolean[N];
+            answer = Integer.MAX_VALUE;
 
             st = new StringTokenizer(br.readLine());
             startX = Integer.parseInt(st.nextToken());
             startY = Integer.parseInt(st.nextToken());
+
             endX = Integer.parseInt(st.nextToken());
             endY = Integer.parseInt(st.nextToken());
 
             for(int i = 0; i < N; i++){
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                house[i][0] = a;
-                house[i][1] = b;
+                houses[i][0] = Integer.parseInt(st.nextToken());
+                houses[i][1] = Integer.parseInt(st.nextToken());
             }
 
-
-
-            perm(0);
+            permutation(0);
 
             System.out.println("#" + t + " " + answer);
         }
     }
 
-    static void perm(int cnt){
+    static void permutation(int cnt){
         if(cnt == N){
             int result = 0;
 
-            result += Math.abs(startX - house[arr[0]][0]) + Math.abs(startY - house[arr[0]][1]);
-            for(int i = 0; i < N - 1; i++){
-                result += Math.abs(house[arr[i]][0]- house[arr[i + 1]][0]) + Math.abs(house[arr[i]][1] - house[arr[i + 1]][1]);
-            }
-            result += Math.abs(endX - house[arr[N - 1]][0]) + Math.abs(endY- house[arr[N - 1]][1]);
+            // 회사에서 첫번째 고객까지
+            result += Math.abs(startX - houses[arr[0]][0]) + Math.abs(startY - houses[arr[0]][1]);
 
-            answer = Math.min(result, answer);
+            // 고객의 좌표
+            for(int i = 0; i < N - 1; i++){
+                int customer = arr[i];
+                int nextCustomer = arr[i + 1];
+
+                result += Math.abs(houses[customer][0] - houses[nextCustomer][0]) + Math.abs(houses[customer][1] - houses[nextCustomer][1]);
+            }
+            
+            // 마지막 고객의 좌표에서 집까지
+            result += Math.abs(houses[arr[N - 1]][0] - endX) + Math.abs(houses[arr[N - 1]][1] - endY);
+
+            answer = Math.min(answer, result);
 
             return;
         }
 
         for(int i = 0; i < N; i++){
-            if(!selected[i]){
-                selected[i] = true;
+            if(!isSelected[i]){
+                isSelected[i] = true;
                 arr[cnt] = i;
-                perm(cnt + 1);
-                selected[i] = false;
+                permutation(cnt + 1);
+                isSelected[i] = false;
             }
         }
     }
