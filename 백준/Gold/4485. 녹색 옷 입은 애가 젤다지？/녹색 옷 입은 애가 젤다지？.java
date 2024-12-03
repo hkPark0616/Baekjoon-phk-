9eec002dc74f27f96a01812d642d4ab57db30a10
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-03 16:23:41 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 152 ms, Memory: 16672 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -4,80 +4,80 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, answer, map[][], cost[][];
     static int INF = Integer.MAX_VALUE;
-    static int[] delX = {-1, 0, 1, 0}, delY = {0, 1, 0, -1};
+    static int N;
+    static int[][] map, dist;
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static StringBuilder sb = new StringBuilder();
+    static class Node implements Comparable<Node>{
+        int x, y, w;
 
-    static class Node implements Comparable<Node> {
-        int from;
-        int to;
-        int weight;
-
-        Node(int from, int to, int weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
+        public Node(int x, int y, int w){
+            this.x = x;
+            this.y = y;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Node node) {
-            return weight - node.weight;
+        public int compareTo(Node o) {
+            return this.w - o.w;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int tc = 1;
+
+        int t = 1;
         while(true){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            answer = 0;
 
             if(N == 0) break;
 
             map = new int[N][N];
-            cost = new int[N][N];
+            dist = new int[N][N];
+
+            for(int[] d: dist) {
+                Arrays.fill(d, INF);
+            }
+
             for(int i = 0; i < N; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < N; j++){
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    cost[i][j] = INF;
                 }
             }
 
-            cost[0][0] = map[0][0];
+            dist[0][0] = map[0][0];
 
-            Dijkstra(0, 0);
-            answer = cost[N - 1][N - 1];
+            Dijkstra();
 
-            sb.append("Problem " + tc++ + ": " + answer + "\n");
+            sb.append("Problem ").append(t++).append(": ").append(dist[N - 1][N - 1] + "\n");
         }
 
         System.out.println(sb.toString());
     }
 
-    static void Dijkstra(int a, int b){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    static void Dijkstra(){
+        PriorityQueue<Node> q = new PriorityQueue<>();
 
-        pq.add(new Node(a, b, map[0][0]));
+        q.offer(new Node(0, 0, map[0][0]));
 
-        while(!pq.isEmpty()){
-            Node current = pq.poll();
-
-            int x = current.from;
-            int y = current.to;
-            int curCost = current.weight;
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            int x = node.x;
+            int y = node.y;
 
             for(int d = 0; d < 4; d++){
-                int nx = x + delX[d];
-                int ny = y + delY[d];
+                int nx = x + deltas[d][0];
+                int ny = y + deltas[d][1];
 
-                if(nx >= 0 && nx < N && ny >= 0 && ny < N){
-                    if(cost[nx][ny] > curCost + map[nx][ny]){
-                        cost[nx][ny] = curCost + map[nx][ny];
-                        pq.offer(new Node(nx, ny, cost[nx][ny]));
+                if(nx >= 0 && ny >= 0 && nx < N && ny < N){
+                    int next = dist[x][y] + map[nx][ny];
+                    if(dist[nx][ny] > next){
+                        dist[nx][ny] = next;
+                        q.offer(new Node(nx, ny, dist[nx][ny]));
                     }
                 }
             }
