860fdf163ee3be2b93bdd8540da2a151efb3ff3d
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-03 10:29:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 188 ms, Memory: 26,180 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -8,18 +8,17 @@ public class Solution {
     static int N, M, R, C, L, answer;
     static int[][] map;
     static boolean[][] visited;
-    static int[] delX = {-1, 1, 0, 0};
-    static int[] delY = {0, 0, -1, 1};
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int[][] pipes = {
-            {0, 0, 0, 0},
-            {1, 1, 1, 1},
-            {1, 1, 0, 0},
-            {0, 0, 1, 1},
-            {1, 0, 0, 1},
-            {0, 1, 0, 1},
-            {0, 1, 1, 0},
-            {1, 0, 1, 0},
-    };
+                                {0, 0, 0, 0},
+                                {1, 1, 1, 1},
+                                {1, 1, 0, 0},
+                                {0, 0, 1, 1},
+                                {1, 0, 0, 1},
+                                {0, 1, 0, 1},
+                                {0, 1, 1, 0},
+                                {1, 0, 1, 0}
+                            };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,11 +28,13 @@ public class Solution {
 
         for(int t = 1; t <= T; t++){
             st = new StringTokenizer(br.readLine());
+
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
             L = Integer.parseInt(st.nextToken());
+
             map = new int[N][M];
             visited = new boolean[N][M];
 
@@ -44,49 +45,52 @@ public class Solution {
                 }
             }
 
+            // 맨홀뚜겅이 위치한 지점을 포함
             answer = 1;
+
             bfs();
 
             System.out.println("#" + t + " " + answer);
-
         }
     }
 
-    static void bfs() {
+    static void bfs(){
         ArrayDeque<int[]> q = new ArrayDeque<>();
 
-        q.offer(new int[] {R, C});  // (R, C) 순서로 넣기
+        // 시작점(맨홀 위치)
         visited[R][C] = true;
-
-        int time = 1; // 경과 시간
-
-        for(int t = 1; t < L; t++) {
+        q.offer(new int[] {R, C});
+        
+        // 주어진 시간만큼 반복
+        for(int i = 1; i < L; i++){
+            // 시간 단위로 탐색하기 위해서
             int size = q.size();
+            for(int s = 0; s < size; s++) {
+                int[] cur = q.poll();
+                int x = cur[0];
+                int y = cur[1];
 
-            for(int i = 0; i < size; i++) {
-                int[] temp = q.poll();
-                int r = temp[0];
-                int c = temp[1];
+                int[] pipe = pipes[map[x][y]];
 
-                int[] pipe = pipes[map[r][c]];
+                for(int d = 0; d < 4; d++){
+                    // 해당 파이프 방향으로 이동할 수 없으면 넘김
+                    if(pipe[d] == 0) continue;
+                    
+                    int nx = x + deltas[d][0];
+                    int ny = y + deltas[d][1];
 
-                for (int d = 0; d < 4; d++) {
-                    if (pipe[d] == 0) continue;
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
 
-                    int nr = r + delX[d];
-                    int nc = c + delY[d];
+                    if(visited[nx][ny] || map[nx][ny] == 0) continue;
 
+                    // 현재의 파이프가 다음 파이프(반대 방향으로)랑 연결되는지
                     int dir = d % 2 == 0 ? d + 1 : d - 1;
-
-                    if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
-
-                    if (visited[nr][nc] || map[nr][nc] == 0) continue;
-
-                    if (pipes[map[nr][nc]][dir] == 1) {
-                        q.offer(new int[] {nr, nc});
-                        visited[nr][nc] = true;
+                    if(pipes[map[nx][ny]][dir] == 1){
+                        q.offer(new int[] {nx, ny});
+                        visited[nx][ny] = true;
                         answer++;
                     }
+
                 }
             }
         }
