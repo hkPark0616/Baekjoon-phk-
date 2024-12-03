9094dd1ca8dc1fba8bce4881d9bafada5f65db39
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-03 11:06:48 +0900

    [D5] Title: 상원이의 생일파티, Time: 182 ms, Memory: 37,092 KB -BaekjoonHub

--- SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
+++ SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int N, M, relations;
+    static int N, M, answer;
     static List<List<Integer>> graph;
     static boolean[] visited;
 
@@ -20,44 +23,48 @@ public class Solution {
             M = Integer.parseInt(st.nextToken());
             graph = new ArrayList<>();
             visited = new boolean[N + 1];
-            for(int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+
+            for(int i = 0; i <= N; i++){
+                graph.add(new ArrayList<>());
+            }
 
             for(int i = 0; i < M; i++){
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
+
                 graph.get(a).add(b);
                 graph.get(b).add(a);
             }
 
-            relations = 0;
-
+            answer = 0;
+            // 상원이(1)부터 시작
             bfs(1);
 
-            System.out.println("#" + t + " " + relations);
+            System.out.println("#" + t + " " + answer);
         }
     }
 
     static void bfs(int start){
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {start, 0});
+        q.offer(new int[] {start, 0}); // 시작지점과 관계 깊이
         visited[start] = true;
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            int depth = cur[1];
 
-            List<Integer> list = graph.get(cur[0]);
+            int n = cur[0];
+            int depth = cur[1]; // 현재 관계 깊이
 
-            for(int i = 0; i < list.size(); i++){
-                int n = list.get(i);
+            List<Integer> list = graph.get(n); // 친구 관계 리스트
 
-                if(!visited[n]){
-                    visited[n] = true;
-                    relations++;
+            for(int relation: list){
+                if(!visited[relation]){
+                    visited[relation] = true;
+                    answer++;
 
-                    if (depth < 1) {
-                        q.offer(new int[] {n, depth + 1});
+                    if(depth < 1){ // 상원이 친구와 친구의 친구까지만
+                        q.offer(new int[] {relation, depth + 1});
                     }
                 }
             }
