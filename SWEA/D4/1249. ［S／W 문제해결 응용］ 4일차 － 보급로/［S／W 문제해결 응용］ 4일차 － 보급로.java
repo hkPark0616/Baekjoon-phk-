5533e1df2337468e681fcaf3c81f6c471b563ecd
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-03 13:23:46 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 141 ms, Memory: 22,904 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,25 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int N, map[][], answer;
-    static List<List<Node>> graph;
-    static int dist[][];
     static int INF = Integer.MAX_VALUE;
-    static int[][] deltas = {
-                                {-1, 0},
-                                {0, 1},
-                                {1, 0},
-                                {0, -1}
-                            };
-    static class Node implements Comparable<Node> {
-        int x;
-        int y;
-        int weight;
+    static int N, answer;
+    static int[][] map, dist;
+    static List<List<Node>> graph;
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static class Node implements Comparable<Node>{
+        int x, y, weight;
 
-        public Node(int x, int y, int weight) {
+        public Node(int x, int y, int weight){
             this.x = x;
             this.y = y;
             this.weight = weight;
@@ -27,72 +23,62 @@ public class Solution {
 
         @Override
         public int compareTo(Node o) {
-            return weight - o.weight;
+            return this.weight - o.weight;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int T = Integer.parseInt(st.nextToken());
 
-        for(int t = 1; t <= T; t++) {
+        for(int t = 1; t <= T; t++){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             map = new int[N][N];
             dist = new int[N][N];
             answer = Integer.MAX_VALUE;
 
-            graph = new ArrayList<>();
-            for(int i = 1; i <= N * N; i++){
-                graph.add(new ArrayList<>());
-            }
-
-            // 거리 배열을 무한대로 초기화
-            for (int i = 0; i < N; i++) {
-                Arrays.fill(dist[i], INF);
-            }
-
-            for(int i = 0; i < N; i++) {
-                String input = br.readLine();
-                for(int j = 0; j < N; j++) {
-                    map[i][j] = input.charAt(j) - '0';
+            for(int i = 0; i < N; i++){
+                String s = br.readLine();
+                for(int j = 0; j < N; j++){
+                    map[i][j] = s.charAt(j) - '0';
                 }
+            }
+
+            for(int[] d: dist){
+                Arrays.fill(d, INF);
             }
 
             Dijkstra(0, 0);
 
-            answer = dist[N - 1][N - 1];
-            System.out.println("#" + t + " " + answer);
+            System.out.println("#" + t + " " + dist[N - 1][N - 1]);
         }
     }
 
-    static void Dijkstra(int startX, int startY) {
+    static void Dijkstra(int startX, int startY){
         PriorityQueue<Node> pq = new PriorityQueue<>();
         boolean[][] visited = new boolean[N][N];
 
         dist[startX][startY] = 0;
-
+        visited[startX][startY] = true;
         pq.offer(new Node(startX, startY, 0));
 
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             Node node = pq.poll();
-            int currentX = node.x;
-            int currentY = node.y;
+            int x = node.x;
+            int y = node.y;
 
-            if(visited[currentX][currentY]) continue;
+            for(int d = 0; d < 4; d++){
+                int nx = x + deltas[d][0];
+                int ny = y + deltas[d][1];
 
-            visited[currentX][currentY] = true;
-
-            for(int d = 0; d < deltas.length; d++) {
-                int nextX = currentX + deltas[d][0];
-                int nextY = currentY + deltas[d][1];
-
-                if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
-                    int next = dist[currentX][currentY] + map[nextX][nextY];
-                    if(dist[nextX][nextY] > next) {
-                        dist[nextX][nextY] = next;
-                        pq.offer(new Node(nextX, nextY, dist[nextX][nextY]));
+                if(nx >= 0 && ny >= 0 && nx < N && ny < N){
+                    int next = dist[x][y] + map[nx][ny];
+                    if(dist[nx][ny] > next){
+                        dist[nx][ny] = next;
+                        pq.offer(new Node(nx, ny, dist[nx][ny]));
                     }
                 }
             }
