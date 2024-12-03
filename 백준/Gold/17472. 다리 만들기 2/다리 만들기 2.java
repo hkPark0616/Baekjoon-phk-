1ede59be4b933b4e48f03d3a23f7b44d99f9e305
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-12-03 14:14:48 +0900

    [Gold I] Title: 다리 만들기 2, Time: 68 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
+    static int island, bridgeCount, answer;
     static int[][] map;
     static boolean[][] visited;
+    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int[] parent;
     static PriorityQueue<Edge> pq;
-    static int island, bridgeCount, answer;
-    static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static class Edge implements Comparable<Edge>{
+        int x, y, w;
 
-    static class Edge implements  Comparable<Edge>{
-        int a, b, w;
-
-        public Edge(int a, int b, int w) {
-            this.a = a;
-            this.b = b;
+        public Edge(int x, int y, int w){
+            this.x = x;
+            this.y = y;
             this.w = w;
         }
 
@@ -36,7 +34,6 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][M];
         visited = new boolean[N][M];
 
@@ -47,6 +44,7 @@ public class Main {
             }
         }
 
+        // 섬 구분짓기
         island = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
@@ -56,43 +54,46 @@ public class Main {
                 }
             }
         }
-
+        
+        // 다리 만들기
         pq = new PriorityQueue<>();
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
                 if(map[i][j] != 0){
-                    findBridge(i, j, map[i][j]);
+                    makeBridge(i, j, map[i][j]);
                 }
             }
         }
-
-        parent = new int[island + 1];
+        
+        // 크루스칼
+        parent = new int[island + 1]; // 섬의 개수(정점)
         for(int i = 0; i < parent.length; i++){
             parent[i] = i;
         }
-
-        answer = 0;
-        bridgeCount = 0;
+        
         int size = pq.size();
+        bridgeCount = 0;
+        answer = 0;
         for(int i = 0; i < size; i++){
             Edge e = pq.poll();
 
-            int a = find(e.a);
-            int b = find(e.b);
+            int a = e.x;
+            int b = e.y;
+            int w = e.w;
 
-            if(a != b){
+            if(!isSame(a, b)){
                 union(a, b);
-                answer += e.w;
+                answer += w;
                 bridgeCount++;
             }
         }
 
+        // 섬이 모두 이어지지 않는다면
         if(answer == 0 || bridgeCount != island - 1){
             System.out.println("-1");
         }else{
             System.out.println(answer);
         }
-
     }
 
     static void union(int a, int b){
@@ -112,11 +113,16 @@ public class Main {
         return parent[a] = find(parent[a]);
     }
 
+    static boolean isSame(int a, int b){
+        return find(a) == find(b);
+    }
+
     static void bfs(int a, int b){
-        Queue<int[]> q = new LinkedList<>();
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+
         visited[a][b] = true;
         map[a][b] = island;
-        q.offer(new int[]{a, b});
+        q.offer(new int[] {a, b});
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
@@ -130,31 +136,32 @@ public class Main {
                 if(nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && map[nx][ny] == 1){
                     visited[nx][ny] = true;
                     map[nx][ny] = island;
-                    q.add(new int[]{nx, ny});
-
+                    q.offer(new int[] {nx, ny});
                 }
             }
         }
     }
-
-    static void findBridge(int a, int b, int num){
+    
+    static void makeBridge(int a, int b, int num){
         for(int d = 0; d < 4; d++){
             int x = a;
             int y = b;
-            int length = 0;
+            int length = 0; // 다리길이
 
             while(true){
-                x += deltas[d][0];
-                y += deltas[d][1];
-
-                if(x < 0 || x >= N || y < 0 || y >= M) break;
-                if(map[x][y] == num) break;
-                if(map[x][y] > 0){
-                    if(length > 1){
-                        pq.offer(new Edge(num, map[x][y], length));
+                x = x + deltas[d][0];
+                y = y + deltas[d][1];
+                
+                if(x < 0 || y < 0 || x >= N || y >= M) break; // 범위
+                if(map[x][y] == num) break; // 같은 섬은 안됨
+                if(map[x][y] > 0){ // 다른 섬일때
+                    if(length > 1) { // 다리 길이가 1 이상
+                        pq.offer(new Edge(num, map[x][y], length)); // 이전 섬부터 도착한 섬까지 다리 길이(비용)
                     }
                     break;
                 }
+
+                // 바다인 경우 계속 다리를 이어나감
                 length++;
             }
         }
