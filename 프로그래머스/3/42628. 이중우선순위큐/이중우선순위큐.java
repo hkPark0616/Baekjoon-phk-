7f6d5e7fb9618c56b32efe9dc5ee9dcb1c60d108
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-06-04 14:57:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.02 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for (String op : operations) {
+            String[] parts = op.split(" ");
+            String command = parts[0];
+            int num = Integer.parseInt(parts[1]);
+
+            if (command.equals("I")) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            } else if (command.equals("D")) {
+                if (map.isEmpty()) continue;
+
+                int key = (num == 1) ? map.lastKey() : map.firstKey();
+
+                if (map.get(key) == 1) {
+                    map.remove(key);
+                } else {
+                    map.put(key, map.get(key) - 1);
+                }
+            }
+        }
+
+        if (map.isEmpty()) {
+            return new int[]{0, 0};
+        }
+
+        return new int[]{map.lastKey(), map.firstKey()};
+    }
+}
