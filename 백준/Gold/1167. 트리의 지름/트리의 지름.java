e8a7c6f25ff72d79f481fc531b87b33ceae557df
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-06-23 17:13:56 +0900

    [Gold II] Title: 트리의 지름, Time: 1020 ms, Memory: 105760 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,70 +1,65 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
+public class Main {
     static int V;
     static List<List<Node>> graph = new ArrayList<>();
     static boolean[] visited;
     static int maxDist;
-    static int farthestNode = 0;
+    static int farthestNode;
+
     static class Node {
         int to, weight;
 
-        public Node(int to, int weight) {
+        Node(int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         V = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i <= V; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-        for(int i = 0; i < V; i++) {
-            st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            
-            while(true) {
-                int to = Integer.parseInt(st.nextToken());
-
-                if(to == -1) break;
-
-                int weight = Integer.parseInt(st.nextToken());
-
+        for (int i = 0; i < V; i++) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int v = Integer.parseInt(tokens[0]);
+            for (int j = 1; j < tokens.length - 1; j += 2) {
+                int to = Integer.parseInt(tokens[j]);
+                if (to == -1) break;
+                int weight = Integer.parseInt(tokens[j + 1]);
                 graph.get(v).add(new Node(to, weight));
-                graph.get(to).add(new Node(v, weight));
             }
-            
-            
         }
 
         visited = new boolean[V + 1];
         dfs(1, 0);
 
-
         visited = new boolean[V + 1];
         maxDist = 0;
         dfs(farthestNode, 0);
 
-        System.out.println(maxDist);
-        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(maxDist));
+        bw.flush();
     }
 
     static void dfs(int cur, int dist) {
         visited[cur] = true;
-        if(dist > maxDist) {
+
+        if (dist > maxDist) {
             maxDist = dist;
             farthestNode = cur;
         }
 
-        for(Node nextNode: graph.get(cur)) {
-            if(!visited[nextNode.to]) {
-                dfs(nextNode.to, dist + nextNode.weight);
+        for (Node next : graph.get(cur)) {
+            if (!visited[next.to]) {
+                dfs(next.to, dist + next.weight);
             }
         }
     }
