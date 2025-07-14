b82de631e64192ecf5427d27312a5d57f3492ee0
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-07-14 20:10:23 +0900

    [Gold III] Title: 중량제한, Time: 324 ms, Memory: 48788 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -2,15 +2,23 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// Kruscal
 class Main {
     static int N, M, maxWeight, start, end;
-    static List<List<Edge>> graph = new ArrayList<>();
-    static class Edge {
-        int to, weight;
+    static int[] parent;
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
+    static class Edge implements Comparable<Edge> {
+        int from, to, weight;
 
-        public Edge(int to, int weight) {
+        public Edge(int from, int to, int weight) {
+            this.from = from;
             this.to = to;
             this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge e) {
+            return Integer.compare(e.weight, this.weight);
         }
     }
     
@@ -20,8 +28,10 @@ class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
 
-        for(int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+        for(int i = 0; i <= N; i++) parent[i] = i;
+        
 
         maxWeight = 0;
 
@@ -31,61 +41,45 @@ class Main {
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
 
-            graph.get(A).add(new Edge(B, C));
-            graph.get(B).add(new Edge(A, C));
-            
-            maxWeight = Math.max(maxWeight, C);
+            pq.offer(new Edge(A, B, C));
         }
 
         st = new StringTokenizer(br.readLine());
         start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
 
-        System.out.println(parametricSearch());
-    }
-
-    // 운반 가능한 최대 중량 찾기(이분탐색)
-    static int parametricSearch() {
-        int left = 1;
-        int right = maxWeight;
         int answer = 0;
+        
+        while(!pq.isEmpty()) {
+            Edge e = pq.poll();
+            union(e.from, e.to);
 
-        while(left <= right) {
-            int mid = (left + right) / 2;
-
-            if(canMove(mid)) {
-                left = mid + 1;
-                answer = mid;
-            } else {
-                right = mid - 1;
+            // 만약 두 공장이 같은 부모를 같는다면 연결됐다 -> 이때의 가중치가 정답
+            if(isSame(start, end)) {
+                answer = e.weight;
+                break;
             }
         }
 
-        return answer;
+        System.out.println(answer);
     }
 
-    // bfs
-    static boolean canMove(int mid) {
-        boolean[] visited = new boolean[N + 1];
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
 
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-
-            if(cur == end) return true;
-
-            for(Edge e: graph.get(cur)) {
-                // mid는 현재 운반 가능한 다리 중량
-                // 지나는 다리들의 중량이 mid 이상이어야 운반 가능
-                if(!visited[e.to] && e.weight >= mid) {
-                    visited[e.to] = true;
-                    q.offer(e.to);
-                }
-            }
+        if(a != b) {
+            parent[b] = a;
         }
-        
-        return false;
+    }
+
+    static int find(int a) {
+        if(a == parent[a]) return parent[a];
+
+        return parent[a] = find(parent[a]);
+    }
+
+    static boolean isSame(int a, int b) {
+        return find(a) == find(b);
     }
 }
