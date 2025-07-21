29f2612ac5f607193da538c6c85e35dcd78560e0
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-07-21 17:32:56 +0900

    [Gold III] Title: 음악프로그램, Time: 120 ms, Memory: 12944 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int N, M;
+    static int[] indegree;
+    static List<List<Integer>> graph = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        indegree = new int[N + 1];
+        for(int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+        
+            int a = Integer.parseInt(st.nextToken());
+        
+            for (int i = 1; i < num; i++) {
+                int b = Integer.parseInt(st.nextToken());
+        
+                graph.get(a).add(b);
+                indegree[b]++;
+                a = b;
+            }
+        }
+
+        topologySort();
+    }
+
+    static void topologySort() {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        List<Integer> results = new ArrayList<>();
+
+        for(int i = 1; i <= N; i++) {
+            if(indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+
+            results.add(cur);
+            
+            for(int n: graph.get(cur)) {
+                indegree[n]--;
+                
+                if(indegree[n] == 0) {
+                    q.offer(n);
+                }
+            }
+        }
+
+        if (results.size() == N) {
+            for (int num : results) System.out.println(num);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
