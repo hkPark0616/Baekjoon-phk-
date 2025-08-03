2f1627a778033f2f92d53dd6f54a6a02f7d7dfef
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-08-03 10:58:25 +0900

    [Gold III] Title: 나무 재테크, Time: 528 ms, Memory: 182712 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -6,26 +6,9 @@ class Main {
     static int N, M, K;
     static int[][] add; // 겨울에 로봇이 추가할 양분
     static Cell[][] map;
-    static List<DeadTree> deadTrees = new ArrayList<>();
     static class Cell {
-        ArrayDeque<Tree> trees = new ArrayDeque<>(); // 각 칸 나무들
+        ArrayDeque<Integer> trees = new ArrayDeque<>(); // 각 칸 나무들
         int nutrition = 5; // 초기 영양 상태
-    }
-    static class Tree {
-        int age;
-        
-        public Tree(int age) {
-            this.age = age;
-        }
-    }
-    static class DeadTree {
-        int x, y, age;
-
-        public DeadTree(int x, int y, int age) {
-            this.x = x;
-            this.y = y;
-            this.age = age;
-        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -57,12 +40,11 @@ class Main {
             int y = Integer.parseInt(st.nextToken()) - 1;
             int z = Integer.parseInt(st.nextToken());
 
-            map[x][y].trees.add(new Tree(z));
+            map[x][y].trees.add(z);
         }
 
         for(int k = 0; k < K; k++) {
-            spring();
-            summer();
+            springAndSummer();
             fall();
             winter();
         }
@@ -77,47 +59,41 @@ class Main {
         System.out.println(cnt);
     }
 
-    static void spring() {
+    static void springAndSummer() {
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                ArrayDeque<Tree> alive = new ArrayDeque<>();
+                ArrayDeque<Integer> alive = new ArrayDeque<>();
+                int nutrition = map[i][j].nutrition;
+                int deadNutrition = 0;
                 
                 while (!map[i][j].trees.isEmpty()) {
-                    Tree tree = map[i][j].trees.pollFirst(); // 어린 나무부터
-                    if (map[i][j].nutrition >= tree.age) {
-                        map[i][j].nutrition -= tree.age;
-                        tree.age++;
-                        alive.addLast(tree);
+                    int age = map[i][j].trees.pollFirst(); // 어린 나무부터
+                    if (nutrition >= age) {
+                        nutrition -= age;
+                        alive.addLast(age + 1);
                     } else {
-                        deadTrees.add(new DeadTree(i, j, tree.age));
+                        deadNutrition += age / 2;
                     }
                 }
-                
+
+                map[i][j].nutrition = nutrition + deadNutrition;
                 map[i][j].trees = alive;
             }            
         }
-    }
-
-    static void summer() {
-        for(DeadTree tree: deadTrees) {
-            map[tree.x][tree.y].nutrition += tree.age / 2;
-        }
-
-        deadTrees.clear();
     }
 
     static void fall() {
         int[][] deltas = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                for(Tree tree: map[i][j].trees) {
-                    if(tree.age % 5 == 0) {
+                for(int age: map[i][j].trees) {
+                    if(age % 5 == 0) {
                         for(int[] delta: deltas) {
                             int ni = i + delta[0];
                             int nj = j + delta[1];
 
                             if(ni >= 0 && ni < N && nj >= 0 && nj < N) {
-                                map[ni][nj].trees.addFirst(new Tree(1));
+                                map[ni][nj].trees.addFirst(1);
                             }
                         }
                     }
