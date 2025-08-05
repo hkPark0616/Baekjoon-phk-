3f5fcdf36e09939de01bc3648044395ffe440d37
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-08-05 14:35:34 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1124 ms, Memory: 94648 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int N, M;
+    static List<List<Integer>> list = new ArrayList<>();
+    static int[] praise;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        praise = new int[N + 1];
+        
+        for(int i = 0; i <= N; i++) list.add(new ArrayList<>());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            int p = Integer.parseInt(st.nextToken());
+            if(i == 1) continue; // 사장은 상사 없음
+            list.get(p).add(i);
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            praise[idx] += p;
+        }
+
+        dfs(1); // 사장부터 시작
+
+        for(int i = 1; i <=N; i++) {
+            System.out.print(praise[i] + " ");
+        }
+    }
+
+    static void dfs(int cur) {
+        for(int n: list.get(cur)) {
+            praise[n] += praise[cur]; // 상사가 받은 칭찬을 부하에게 더함
+            dfs(n); // 해당 부하의 부하에게 칭찬 전파
+        }
+    }
+}
