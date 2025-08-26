3d260ba24874c2fc007579300f74e9aba1b707aa
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-08-26 12:16:14 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 76 ms, Memory: 12756 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int N, M, k;
+    static int[] svc = new int[3];
+    static int[][][] hp = new int[61][61][61];
+    static int[][] damages = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1}, {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            svc[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i <= 60; i++) {
+            for (int j = 0; j <= 60; j++) {
+                  Arrays.fill(hp[i][j], -1);       
+            }        
+        }
+
+        bfs();
+    }
+
+    static void bfs() {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        hp[svc[0]][svc[1]][svc[2]] = 0;
+        q.offer(new int[] {svc[0], svc[1], svc[2]});
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int a = cur[0];
+            int b = cur[1];
+            int c = cur[2];
+
+            if(a <= 0 && b <= 0 && c <= 0) {
+                System.out.println(hp[a][b][c]);
+                return;
+            }
+
+            for(int[] damage: damages) {
+                int na = Math.max(0, a - damage[0]);
+                int nb = Math.max(0, b - damage[1]);
+                int nc = Math.max(0, c - damage[2]);
+
+                if(hp[na][nb][nc] == -1) {
+                    hp[na][nb][nc] = hp[a][b][c] + 1;
+                    q.offer(new int[] {na, nb, nc});
+                }
+            }
+        }
+    }
+}
