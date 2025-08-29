b67036c80f56d47306ab59a09682c73ffc4b10c6
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-08-29 15:47:01 +0900

    [Gold IV] Title: 돌 그룹, Time: 176 ms, Memory: 39812 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int A, B, C, sum;
+    static boolean[][] visited = new boolean[1501][1501];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        sum = A + B + C;
+        
+        System.out.println(bfs(A, B, C));
+    }
+
+    static int bfs(int a, int b, int c) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {a, b});
+        visited[a][b] = true;
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int z = sum - x - y;
+
+            if(x == y && y == z) return 1;
+
+            int[] stones = {x, y, z};
+
+            for(int i = 0; i < 3; i++) {
+                for(int j = i + 1; j < 3; j++) {
+                    int X = stones[i];
+                    int Y = stones[j];
+
+                    if(X == Y) continue;
+
+                    if(X < Y) {
+                        int nx = X * 2;
+                        int ny = Y - X;
+                        int nz = sum - nx - ny;
+
+                        if(!visited[nx][ny]) {
+                            visited[nx][ny] = true;
+                            q.offer(new int[] {nx, ny});
+                        }
+                    } else {
+                        int nx = Y * 2;
+                        int ny = X - Y;
+                        int nz = sum - nx - ny;
+
+                        if(!visited[nx][ny]) {
+                            visited[nx][ny] = true;
+                            q.offer(new int[] {nx, ny});
+                        }
+                    }
+                }
+            }
+        }
+        
+        return 0;
+    }
+}
