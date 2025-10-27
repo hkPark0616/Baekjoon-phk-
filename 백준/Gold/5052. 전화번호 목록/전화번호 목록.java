d1feb7e542d1d8ed3a0c2ae76f51176f40f257c3
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-10-27 20:47:49 +0900

    [Gold IV] Title: 전화번호 목록, Time: 512 ms, Memory: 31892 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -3,44 +3,6 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static class Node {
-        Map<Character, Node> child;
-        boolean endOfWord;
-
-        public Node() {
-            this.child = new HashMap<>();
-            this.endOfWord = false;
-        }
-    }
-    static class Trie {
-        Node root;
-
-        public Trie() {
-            this.root = new Node();
-        }
-
-        // 전화번호 삽입 중 접두어 관계 발견 시 false 반환
-        public boolean insert(String str) {
-            Node node = this.root;
-
-            for(int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-
-                if(node.endOfWord) return false; // 이미 끝난 단어를 거친다면 → 기존 번호가 접두어
-                
-                node.child.putIfAbsent(c, new Node());
-                node = node.child.get(c);
-            }
-
-            // 마지막 글자 처리 후 자식이 있으면 → 현재 번호가 다른 번호의 접두어
-            if(!node.child.isEmpty()) return false;
-            
-            node.endOfWord = true;
-
-            return true;
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -55,11 +17,9 @@ class Main {
 
             Arrays.sort(nums); // 짧은 번호를 먼저 삽입하기 위해서
 
-            Trie trie = new Trie();
             boolean answer = true;
-
-            for(String num: nums) {
-                if(!trie.insert(num)) {
+            for (int i = 0; i < N - 1; i++) {
+                if(nums[i + 1].startsWith(nums[i])) {
                     answer = false;
                     break;
                 }
@@ -69,6 +29,5 @@ class Main {
         }
 
         System.out.println(sb);
-        
     }
 }
