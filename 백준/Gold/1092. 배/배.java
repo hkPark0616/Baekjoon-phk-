b7467d38f0c9328f7e20dba2cda277c0e0447679
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2025-10-29 10:38:00 +0900

    [Gold V] Title: 배, Time: 412 ms, Memory: 14972 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Integer[] cranes = new Integer[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) cranes[i] = Integer.parseInt(st.nextToken());
+
+        int M = Integer.parseInt(br.readLine());
+        Integer[] boxes = new Integer[M];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++) boxes[i] = Integer.parseInt(st.nextToken());
+
+        // 제일 센 크레인이 제일 무거운 박스를 먼저 들게
+        Arrays.sort(cranes, Collections.reverseOrder());
+        Arrays.sort(boxes, Collections.reverseOrder());
+
+        if(boxes[0] > cranes[0]) { // 가장 센 크레인이 가장 무거운 박스를 못들면 모두 못옮김
+            System.out.println(-1);
+            return;
+        }
+        
+        boolean[] moved = new boolean[M];
+        int cnt = 0;
+        int time = 0;
+        while(cnt < M) {
+            int idx = 0;
+            for(int i = 0; i < N; i++) {
+                while(idx < M) {
+                    if(!moved[idx] && cranes[i] >= boxes[idx]) {
+                        moved[idx] = true;
+                        cnt++;
+                        idx++;
+                        break;
+                    }
+                    idx++;
+                }
+            }
+            time++;
+        }
+
+        System.out.println(time);
+    }
+
+}
